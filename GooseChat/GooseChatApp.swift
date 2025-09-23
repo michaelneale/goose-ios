47a465011299786d13a1f2f4cd170d86a88f4bf2
Michael Neale <michael.neale@gmail.com>
2025-09-23 17:16:35 +1000

    can read QR codes for url handler

--- GooseChat/GooseChatApp.swift
+++ GooseChat/GooseChatApp.swift
@@ -2,9 +2,16 @@ import SwiftUI
 
 @main
 struct GooseChatApp: App {
+    @StateObject private var configurationHandler = ConfigurationHandler.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(configurationHandler)
+                .onOpenURL { url in
+                    print("📱 App received URL: \(url)")
+                    _ = configurationHandler.handleURL(url)
+                }
         }
     }
 }
